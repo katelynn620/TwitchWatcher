3e7356f08b55d40bf48a84b0c873ea1f6793d188
Kiyomi Matsumoto <hiru_0@yahoo.co.jp>
2025-01-30 15:22:14 +0000

    fix: Clean up

--- Bot.cs
+++ Bot.cs
@@ -1,8 +1,5 @@
-using System;
 using TwitchLib.Client;
-using TwitchLib.Client.Enums;
 using TwitchLib.Client.Events;
-using TwitchLib.Client.Extensions;
 using TwitchLib.Client.Models;
 using TwitchLib.Communication.Clients;
 using TwitchLib.Communication.Models;
@@ -23,28 +20,34 @@ class Bot
         };
         WebSocketClient customClient = new WebSocketClient(clientOptions);
         client = new TwitchClient(customClient);
-        client.Initialize(credentials, username);
 
-        client.OnLog += Client_OnLog;
+        client.Initialize(credentials, new List<string>([username]));
+
+        // client.OnLog += Client_OnLog;
         client.OnJoinedChannel += Client_OnJoinedChannel;
-        client.OnMessageReceived += Client_OnMessageReceived;
-        client.OnWhisperReceived += Client_OnWhisperReceived;
-        client.OnNewSubscriber += Client_OnNewSubscriber;
         client.OnConnected += Client_OnConnected;
-        client.OnMessageSent += Client_OnMessageSent;
+        client.OnUserJoined += Client_OnUserJoined;
+        client.OnUserLeft += Client_OnUserLeft;
 
         client.Connect();
+
+    }
+
+    private void Client_OnUserJoined(object? sender, OnUserJoinedArgs e)
+    {
+        Console.WriteLine($"{e.Username} joined channel {e.Channel}");
+    }
+
+    private void Client_OnUserLeft(object? sender, OnUserLeftArgs e)
+    {
+        Console.WriteLine($"{e.Username} left channel {e.Channel}");
     }
 
     private void Client_OnLog(object? sender, OnLogArgs e)
     {
-        Console.WriteLine($"{e.DateTime.ToString()}: {e.BotUsername} - {e.Data}");
+        Console.WriteLine($"{e.DateTime}: {e.BotUsername} - {e.Data}");
     }
 
-    private void Client_OnMessageSent(object? sender, OnMessageSentArgs e)
-    {
-        Console.WriteLine($"sent message: {e.SentMessage.Message}");
-    }
     private void Client_OnConnected(object? sender, OnConnectedArgs e)
     {
         Console.WriteLine($"Connected to {e.AutoJoinChannel}");
@@ -52,28 +55,7 @@ class Bot
 
     private void Client_OnJoinedChannel(object? sender, OnJoinedChannelArgs e)
     {
-        Console.WriteLine("Hey guys! I am a bot connected via TwitchLib!");
-        client.SendMessage(e.Channel, "Hey guys! I am a bot connected via TwitchLib!");
-    }
-
-    private void Client_OnMessageReceived(object? sender, OnMessageReceivedArgs e)
-    {
-        if (e.ChatMessage.Message.Contains("badword"))
-            client.TimeoutUser(e.ChatMessage.Channel, e.ChatMessage.Username, TimeSpan.FromMinutes(30), "Bad word! 30 minute timeout!");
-    }
-
-    private void Client_OnWhisperReceived(object? sender, OnWhisperReceivedArgs e)
-    {
-        if (e.WhisperMessage.Username == "my_friend")
-            client.SendWhisper(e.WhisperMessage.Username, "Hey! Whispers are so cool!!");
-    }
-
-    private void Client_OnNewSubscriber(object? sender, OnNewSubscriberArgs e)
-    {
-        if (e.Subscriber.SubscriptionPlan == SubscriptionPlan.Prime)
-            client.SendMessage(e.Channel, $"Welcome {e.Subscriber.DisplayName} to the substers! You just earned 500 points! So kind of you to use your Twitch Prime on this channel!");
-        else
-            client.SendMessage(e.Channel, $"Welcome {e.Subscriber.DisplayName} to the substers! You just earned 500 points!");
+        Console.WriteLine($"Channel joined: {e.Channel}");
     }
 }
 
